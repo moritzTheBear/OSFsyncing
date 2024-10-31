2b37506c0629e68148ac52f5dd39be0feed80811
moritzTheBear <166619547+moritzTheBear@users.noreply.github.com>
2024-10-31 10:56:18 +0100

    sh script creates powershell files aswell

--- initializeOSF.sh
+++ initializeOSF.sh
@@ -2,7 +2,7 @@ pip install osfclient
 cd "$(dirname "$0")"
 osf init
 # Create a script to upload files to OSF
-cat << 'EOF' > updaten.command
+cat << 'EOF' > updaten_mac.command
 #!/bin/bash
 echo "Enter your osf-username:"
 read nutzername
@@ -16,13 +16,32 @@ for item in $(osf -u $nutzername ls); do
 done
 osf -u $nutzername upload -r hierUploaden/. Files
 EOF
+#create powershell script to upload files to OSF
+cat << 'EOF' > updaten_win.ps1
+$nutzername = Read-Host "Enter your OSF username"
+$passwort = Read-Host -AsSecureString "Enter your OSF password"
+$env:OSF_PASSWORD = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($passwort))
+Write-Output "Thank you, this will now take a moment"
+cd "$PSScriptRoot"
+
+# List and remove existing files
+foreach ($item in (osf -u $nutzername ls)) {
+    osf -u $nutzername remove $item
+}
+
+# Upload new files
+osf -u $nutzername upload -r "hierUploaden/." "Files"
+EOF
 # Create a script to download the OSF client
-cat << 'EOF' > osfclientInstall.command
+cat << 'EOF' > osfclientInstall_mac.command
 #!/bin/bash
 pip install osfclient
 EOF
 
-chmod +x updaten.command
+# create a powershell script to download the OSF client
+cat << 'EOF' > osfclientInstall_win.ps1
+pip install osfclient
+EOF
 mkdir hierUploaden
 # Delete the script itself
 rm -- "$0"
