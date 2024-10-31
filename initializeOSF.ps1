2e25eb2ec9bdcf59b5c5f15dbf1e553afe0e0726
moritzTheBear <166619547+moritzTheBear@users.noreply.github.com>
2024-10-31 10:05:39 +0100

    windows creates win and mac files

--- initializeOSF.ps1
+++ initializeOSF.ps1
@@ -16,13 +16,35 @@ for /f "tokens=*" %%i in ('osf -u %nutzername% ls') do (
     osf -u %nutzername% remove "%%i"
 )
 osf -u %nutzername% upload -r hierUploaden/. Files
-'@ > updaten.ps1
+'@ > updaten_win.ps1
+# Create a script to upload files to OSF for macOS
+@'
+#!/bin/bash
+read -p "Enter your osf-username: " nutzername
+read -sp "Enter your osf-password: " passwort
+export OSF_PASSWORD=$passwort
+echo "\nThank you, this will now take a moment"
+cd "$(dirname "$0")"
+osf -u $nutzername ls | while read -r line; do
+    osf -u $nutzername remove "$line"
+done
+osf -u $nutzername upload -r hierUploaden/. Files
+'@ > updaten_mac.command
+
+chmod +x updaten_mac.command
 
 # Create a script to download the OSF client
 @'
 @echo off
 pip install osfclient
-'@ > osfclientInstall.ps1
+'@ > osfclientInstall_win.ps1
+# Create a script to download the OSF client for macOS
+@'
+#!/bin/bash
+pip install osfclient
+'@ > osfclientInstall_mac.command
+
+chmod +x osfclientInstall_mac.command
 
 New-Item -ItemType Directory -Force -Path hierUploaden
 Remove-Item -Force -LiteralPath $MyInvocation.MyCommand.Definition
