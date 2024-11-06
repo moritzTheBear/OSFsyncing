6a3166e65e92c88fc6035df7843dc81fbf906cc1
moritzTheBear <166619547+moritzTheBear@users.noreply.github.com>
2024-11-06 09:23:07 +0100

    sh erstellung nicht mehr im initialize.ps1, hierUploaden folder nur wenn noch keiner existiert

--- initializeOSF.ps1
+++ initializeOSF.ps1
@@ -2,6 +2,7 @@ pip install osfclient
 Set-Location -Path (Split-Path -Parent $MyInvocation.MyCommand.Definition)
 osf init
 
+
 # Create a script to upload files to OSF
 @'
 $nutzername = Read-Host "Enter your OSF username"
@@ -18,31 +19,15 @@ foreach ($item in (osf -u $nutzername ls)) {
 # Upload new files
 osf -u $nutzername upload -r "hierUploaden/." "Files"
 '@ > updaten_win.ps1
-# Create a script to upload files to OSF for macOS
-@'
-#!/bin/bash
-read -p "Enter your osf-username: " nutzername
-read -sp "Enter your osf-password: " passwort
-export OSF_PASSWORD=$passwort
-echo "\nThank you, this will now take a moment"
-cd "$(dirname "$0")"
-osf -u $nutzername ls | while read -r line; do
-    osf -u $nutzername remove "$line"
-done
-osf -u $nutzername upload -r hierUploaden/. Files
-'@ > updaten_mac.sh
 
 
 # Create a script to download the OSF client
 @'
 pip install osfclient
 '@ > osfclientInstall_win.ps1
-# Create a script to download the OSF client for macOS
-@'
-#!/bin/bash
-pip install osfclient
-'@ > osfclientInstall_mac.sh
 
 
-New-Item -ItemType Directory -Force -Path hierUploaden
+if (-Not (Test-Path -Path "hierUploaden")) {
+    New-Item -ItemType Directory -Force -Path hierUploaden
+}
 Remove-Item -Force -LiteralPath $MyInvocation.MyCommand.Definition
