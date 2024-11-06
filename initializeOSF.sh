0c5b4577f1ce16d86ff875a34edd494a7fe96539
moritzTheBear <166619547+moritzTheBear@users.noreply.github.com>
2024-11-06 09:40:10 +0100

    windows dateien entfernt und ordnererstellung unter bedingung

--- initializeOSF.sh
+++ initializeOSF.sh
@@ -16,32 +16,15 @@ for item in $(osf -u $nutzername ls); do
 done
 osf -u $nutzername upload -r hierUploaden/. Files
 EOF
-#create powershell script to upload files to OSF
-cat << 'EOF' > updaten_win.ps1
-$nutzername = Read-Host "Enter your OSF username"
-$passwort = Read-Host -AsSecureString "Enter your OSF password"
-$env:OSF_PASSWORD = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($passwort))
-Write-Output "Thank you, this will now take a moment"
-cd "$PSScriptRoot"
-
-# List and remove existing files
-foreach ($item in (osf -u $nutzername ls)) {
-    osf -u $nutzername remove $item
-}
-
-# Upload new files
-osf -u $nutzername upload -r "hierUploaden/." "Files"
-EOF
 # Create a script to download the OSF client
 cat << 'EOF' > osfclientInstall_mac.sh
 #!/bin/bash
 pip install osfclient
 EOF
 
-# create a powershell script to download the OSF client
-cat << 'EOF' > osfclientInstall_win.ps1
-pip install osfclient
-EOF
-mkdir hierUploaden
+
+if [ ! -d "hierUploaden" ]; then
+    mkdir hierUploaden
+fi
 # Delete the script itself
 rm -- "$0"
